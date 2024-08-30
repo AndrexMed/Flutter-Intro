59616c6d0cd435ac28b364945f836e2c1f4d93f0
GiovanniAndrex <giovanniandrex@gmail.com>
2024-08-30 18:29:25 -0500

    Estructura de directorios

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world_app/presentation/screens/counter_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) { 
     return const MaterialApp(
       // debugShowCheckedModeBanner: false, Para desactivar la Etiqueta debug en la app
-        home: Scaffold(
-      body: Center(child: Text('Hello World')),
-    ));
+      home: CounterScreen(),
+    );
   }
 }
